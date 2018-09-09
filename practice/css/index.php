2990d0c30ab39eca54f09fe8dfdb93e06f959c5a
David Flynn <daflynn@csumb.edu>
2018-09-09 17:41:17 +0000

    added css precedence testing

---
+++ practice/css/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>CSS Examples</title>
+        <link href="./sheet1.css" rel = "stylesheet">
+    </head>
+    <body>
+    
+        <img style="width:500px" src="download.png">
+        <style>
+            img {
+                
+                border: 20px groove #2f6cce;
+            }
+            p {
+                margin: 10px;
+            }
+        
+        </style>
+       
+        <p class="highlighted">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tempor vulputate sapien non
+            dignissim. Sed vel pretium odio. Fusce sit amet accumsan dolor. Sed semper fermentum molestie. 
+            Suspendisse sagittis eros eget sem lobortis, a luctus risus dapibus. Proin pulvinar sapien ac 
+            viverra pulvinar. Nullam gravida dui dapibus, rhoncus dui vitae, tempus lorem. Sed venenatis 
+            nisi enim, in lacinia nisi efficitur eget. Mauris neque leo, gravida ac elementum nec, 
+            porttitor vel sapien.
+        </p>
+        <p style = "background-color: orange"  id = "first">
+            Maecenas quis eros et leo sollicitudin sagittis. Integer suscipit justo nec ligula auctor venenatis. Pellentesque fermentum, mauris a volutpat convallis, mauris augue lacinia tortor, et iaculis urna urna eu turpis. Pellentesque eu sollicitudin ex. Cras imperdiet nulla non ullamcorper vehicula. Donec metus dui, pharetra eu tincidunt vitae, ullamcorper vel urna. Praesent ex enim, lobortis at diam id, varius venenatis ante. Praesent dictum suscipit dolor vitae imperdiet. Nulla mattis ligula sit amet fringilla pharetra. Vivamus at sapien in eros tempus pretium nec vel tortor. Duis condimentum vel leo at consequat.
+        </p>
+        
+        <p>
+            Phasellus et neque vel urna malesuada ullamcorper eget eget felis. Sed mauris neque, suscipit ut est sed, suscipit dignissim purus. Nam vulputate malesuada eros et luctus. Integer eleifend augue eget nisi varius eleifend. Nam congue tortor eget lectus volutpat, a ultricies dui elementum. Integer eu elementum nisi. Quisque rutrum pretium venenatis. Integer eu turpis lorem. Nunc porta semper tristique. Nullam eget pharetra est, sed iaculis ipsum. Proin consequat enim tortor, sit amet hendrerit nibh porttitor a. Mauris malesuada leo vitae fermentum sollicitudin. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Phasellus nec nibh nulla. Fusce justo mi, congue a auctor sit amet, egestas nec lectus. Fusce mattis eros eget urna mollis, eget tincidunt libero dignissim.
+        </p>
+    </body>
+</html>
+
